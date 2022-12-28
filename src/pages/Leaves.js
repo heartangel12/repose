543b973eee3ac579ddd06166f5611199630a092f
heartangel01 <202010985@gordoncollege.edu.ph>
2022-12-28 15:05:54 +0800

    added leaves

--- src/pages/Leaves.js
+++ src/pages/Leaves.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "../style/leaves.css";
 
 const Leaves = () => {
-    return (
-        <div>
-            <h1>Leaves</h1>
+  return (
+    <div>
+      <div className="leaves">
+        <div className="profile">
+            <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png" alt="profile"></img>
+            <p>Name...</p>
         </div>
-    );
-}
+        <div className="leave-type">
+          <p>Leave Type</p>
+        </div>
+        <button>Open</button>
+      </div>
+    </div>
+  );
+};
 
 export default Leaves;
